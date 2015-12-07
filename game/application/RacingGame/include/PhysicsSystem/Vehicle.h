c19861833cb476b3894c1e23b2e0de7112a52575
Bastian Jonas <bastian.jonas@rwth-aachen.de>
2015-12-07 13:17:31 +0100

    Updated .gitignore, added empty Vehicle class

---
+++ game/application/RacingGame/include/PhysicsSystem/Vehicle.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <btBulletDynamicsCommon.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/quaternion.hpp>
+
+BEGINNAMESPACE
+
+using Vector3 = glm::vec3;
+using Quaternion = glm::quat;
+
+class Vehicle /*: Entity*/ {
+
+};
+
+ENDNAMESPACE
